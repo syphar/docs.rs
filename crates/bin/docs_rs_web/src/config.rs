193223c4478b783fdc754bb8f9546089df7a0da5
Denis Cornehl <denis@cornehl.org>
2026-01-03 06:20:12 +0100

    wip: bon config web

--- crates/bin/docs_rs_web/src/config.rs
+++ crates/bin/docs_rs_web/src/config.rs
@@ -1,18 +1,26 @@
 use docs_rs_env_vars::{env, maybe_env, require_env};
 use std::{path::PathBuf, time::Duration};
 
-#[derive(Debug)]
+#[derive(Debug, bon::Builder)]
+#[builder(on(_, overwritable))]
 pub struct Config {
     // Access token for APIs for crates.io (careful: use
     // constant_time_eq for comparisons!)
     pub(crate) cratesio_token: Option<String>,
+
     // request timeout in seconds
     pub(crate) request_timeout: Option<Duration>,
+    #[builder(default)]
     pub(crate) report_request_timeouts: bool,
+
     // The most memory that can be used to parse an HTML file
+    #[builder(default = 5 * 1024 * 1024 as usize)]
     pub(crate) max_parse_memory: usize,
+
     /// amount of threads for CPU intensive rendering
+    #[builder(default = num_cpus::get())]
     pub(crate) render_threads: usize,
+
     // random crate search generates a number of random IDs to
     // efficiently find a random crate with > 100 GH stars.
     // The amount depends on the ratio of crates with >100 stars
@@ -20,9 +28,11 @@ pub struct Config {
     // At the time of creating this setting, it is set to
     // `500` for a ratio of 7249 over 54k crates.
     // For unit-tests the number has to be higher.
+    #[builder(default = 500u32)]
     pub(crate) random_crate_search_view_size: u32,
 
     // Content Security Policy
+    #[builder(default)]
     pub(crate) csp_report_only: bool,
 
     // Cache-Control header, for versioned URLs.
@@ -33,32 +43,31 @@ pub struct Config {
     // Activate full page caching.
     // When disabled, we still cache static assets.
     // This only affects pages that depend on invalidations to work.
+    #[builder(default = true)]
     pub(crate) cache_invalidatable_responses: bool,
-
-    pub(crate) storage: docs_rs_storage::Config,
-    pub(crate) build_limits_config: docs_rs_build_limits::Config,
 }
 
 impl Config {
     pub fn from_environment() -> anyhow::Result<Self> {
-        let _prefix: PathBuf = require_env("DOCSRS_PREFIX")?;
-        Ok(Self {
-            cratesio_token: maybe_env("DOCSRS_CRATESIO_TOKEN")?,
+        Ok(Self::builder()
+            .maybe_cratesio_token(maybe_env("DOCSRS_CRATESIO_TOKEN")?)
             // LOL HTML only uses as much memory as the size of the start tag!
             // https://github.com/rust-lang/docs.rs/pull/930#issuecomment-667729380
-            max_parse_memory: env("DOCSRS_MAX_PARSE_MEMORY", 5 * 1024 * 1024)?,
-            render_threads: env("DOCSRS_RENDER_THREADS", num_cpus::get())?,
-            request_timeout: maybe_env::<u64>("DOCSRS_REQUEST_TIMEOUT")?.map(Duration::from_secs),
-            report_request_timeouts: env("DOCSRS_REPORT_REQUEST_TIMEOUTS", false)?,
-            random_crate_search_view_size: env("DOCSRS_RANDOM_CRATE_SEARCH_VIEW_SIZE", 500)?,
-            csp_report_only: env("DOCSRS_CSP_REPORT_ONLY", false)?,
-            cache_control_stale_while_revalidate: maybe_env(
+            .maybe_max_parse_memory(maybe_env("DOCSRS_MAX_PARSE_MEMORY")?)
+            .maybe_render_threads(maybe_env("DOCSRS_RENDER_THREADS")?)
+            .maybe_request_timeout(
+                maybe_env::<u64>("DOCSRS_REQUEST_TIMEOUT")?.map(Duration::from_secs),
+            )
+            .maybe_report_request_timeouts(maybe_env("DOCSRS_REPORT_REQUEST_TIMEOUTS")?)
+            .maybe_random_crate_search_view_size(maybe_env("DOCSRS_RANDOM_CRATE_SEARCH_VIEW_SIZE")?)
+            .maybe_csp_report_only(maybe_env("DOCSRS_CSP_REPORT_ONLY")?)
+            .maybe_cache_control_stale_while_revalidate(maybe_env(
                 "CACHE_CONTROL_STALE_WHILE_REVALIDATE",
-            )?,
-            cache_invalidatable_responses: env("DOCSRS_CACHE_INVALIDATEABLE_RESPONSES", true)?,
-            storage: docs_rs_storage::Config::from_environment()?,
-            build_limits_config: docs_rs_build_limits::Config::from_environment()?,
-        })
+            )?)
+            .maybe_cache_invalidatable_responses(maybe_env(
+                "DOCSRS_CACHE_INVALIDATEABLE_RESPONSES",
+            )?)
+            .build())
     }
 
     #[cfg(test)]
