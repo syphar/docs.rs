50131b048ff018db87b199b475624df263cfb055
Denis Cornehl <denis@cornehl.org>
2026-01-10 06:02:02 +0100

    WIP

--- crates/lib/docs_rs_config/src/lib.rs
+++ crates/lib/docs_rs_config/src/lib.rs
@@ -11,3 +11,41 @@ pub trait AppConfig: Sized {
         Self::from_environment()
     }
 }
+
+pub trait EnvConfigBuilder: Sized {
+    type Config;
+    type Loaded: BuildConfig<Config = Self::Config>;
+
+    fn load_environment(self) -> Result<Self::Loaded>;
+
+    #[cfg(feature = "testing")]
+    fn test_config(self) -> Result<Self::Loaded> {
+        self.load_environment()
+    }
+}
+
+pub trait BuildConfig: Sized {
+    type Config;
+
+    fn build(self) -> Result<Self::Config>;
+}
+
+pub trait HasBuilder: Sized {
+    type Builder: EnvConfigBuilder<Config = Self>;
+
+    fn builder() -> Result<Self::Builder>;
+}
+
+impl<T> AppConfig for T
+where
+    T: HasBuilder,
+{
+    fn from_environment() -> Result<Self> {
+        T::builder()?.load_environment()?.build()
+    }
+
+    #[cfg(feature = "testing")]
+    fn test_config() -> Result<Self> {
+        T::builder()?.test_config()?.build()
+    }
+}
--- crates/lib/docs_rs_database/src/config.rs
+++ crates/lib/docs_rs_database/src/config.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use docs_rs_config::AppConfig;
+use docs_rs_config::{BuildConfig, EnvConfigBuilder, HasBuilder};
 use docs_rs_env_vars::{maybe_env, require_env};
 use url::Url;
 
@@ -17,16 +17,19 @@ pub struct Config {
 
 use config_builder::{SetDatabaseUrl, State};
 
-impl<S: State> ConfigBuilder<S> {
-    pub(crate) fn load_environment(self) -> Result<ConfigBuilder<SetDatabaseUrl<S>>> {
+impl<S: State> EnvConfigBuilder for ConfigBuilder<S> {
+    type Config = Config;
+    type Loaded = ConfigBuilder<SetDatabaseUrl<S>>;
+
+    fn load_environment(self) -> Result<Self::Loaded> {
         Ok(self
             .database_url(require_env("DOCSRS_DATABASE_URL")?)
             .maybe_min_pool_idle(maybe_env("DOCSRS_MIN_POOL_IDLE")?)
             .maybe_max_pool_size(maybe_env("DOCSRS_MAX_POOL_SIZE")?))
     }
 
-    #[cfg(any(test, feature = "testing"))]
-    pub(crate) fn test_config(self) -> Result<ConfigBuilder<SetDatabaseUrl<S>>> {
+    #[cfg(feature = "testing")]
+    fn test_config(self) -> Result<Self::Loaded> {
         Ok(self
             .load_environment()?
             // Use less connections for each test compared to production.
@@ -35,13 +38,18 @@ impl<S: State> ConfigBuilder<S> {
     }
 }
 
-impl AppConfig for Config {
-    fn from_environment() -> Result<Self> {
-        Ok(Self::builder().load_environment()?.build())
-    }
+impl<S: State> BuildConfig for ConfigBuilder<SetDatabaseUrl<S>> {
+    type Config = Config;
 
-    #[cfg(any(test, feature = "testing"))]
-    fn test_config() -> Result<Self> {
-        Ok(Self::builder().test_config()?.build())
+    fn build(self) -> Result<Self::Config> {
+        Ok(self.build())
+    }
+}
+
+impl HasBuilder for Config {
+    type Builder = ConfigBuilder;
+
+    fn builder() -> Result<Self::Builder> {
+        Ok(Self::builder())
     }
 }
