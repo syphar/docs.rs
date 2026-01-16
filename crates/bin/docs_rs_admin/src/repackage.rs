0d78db3060f1bf25caf9116d3845121647730c72
Denis Cornehl <denis@cornehl.org>
2026-01-16 04:48:52 +0100

    kjlk

--- crates/bin/docs_rs_admin/src/repackage.rs
+++ crates/bin/docs_rs_admin/src/repackage.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context as _, Result};
 use docs_rs_storage::{
     AsyncStorage, FileEntry, add_path_into_remote_archive, rustdoc_archive_path,
     source_archive_path,
@@ -10,7 +10,7 @@ use tokio::{fs, io};
 
 async fn repackage(storage: &AsyncStorage, name: &KrateName, version: &Version) -> Result<()> {
     let rustdoc_prefix = format!("rustdoc/{name}/{version}");
-    let sources_prefix = format!("sources/{name}/{version}");
+    // let sources_prefix = format!("sources/{name}/{version}");
 
     repackage_path(
         storage,
@@ -19,12 +19,12 @@ async fn repackage(storage: &AsyncStorage, name: &KrateName, version: &Version) 
     )
     .await?;
 
-    repackage_path(
-        storage,
-        &sources_prefix,
-        &source_archive_path(&name, version),
-    )
-    .await?;
+    // repackage_path(
+    //     storage,
+    //     &sources_prefix,
+    //     &source_archive_path(&name, version),
+    // )
+    // .await?;
 
     // TODO:
     // * fill in `source_size` in release? ( new metric, not in old builds )
@@ -33,7 +33,7 @@ async fn repackage(storage: &AsyncStorage, name: &KrateName, version: &Version) 
     // * releases gets algs from (source, doc)
 
     storage.delete_prefix(&rustdoc_prefix).await?;
-    storage.delete_prefix(&sources_prefix).await?;
+    // storage.delete_prefix(&sources_prefix).await?;
 
     Ok(())
 }
@@ -43,32 +43,53 @@ async fn repackage_path(
     prefix: &str,
     target_archive: &str,
 ) -> Result<(Vec<FileEntry>, CompressionAlgorithm)> {
+    let prefix = format!("{}/", prefix.trim_end_matches('/'));
     let tempdir = spawn_blocking(|| tempfile::tempdir().map_err(Into::into)).await?;
 
-    let mut list = storage.list_prefix(prefix).await;
+    // TODO: optimize: directly pack into zip , don't store locally first.
 
-    let mut to_delete = Vec::new();
-
+    let mut list = storage.list_prefix(&prefix).await;
     while let Some(entry) = list.next().await {
-        let entry = entry?;
+        let entry = dbg!(entry?);
+        let mut stream = storage
+            .get_stream(&entry)
+            .await
+            .context("error getting stream")?;
 
-        let mut stream = storage.get_stream(&entry).await?;
-        to_delete.push(stream.path.clone());
+        let target_path = dbg!(tempdir.path().join(stream.path.trim_start_matches(&prefix)));
 
-        let target_path = tempdir.path().join(stream.path);
-
-        // TODO: optimize: directly pack into zip etc.
+        fs::create_dir_all(&target_path.parent().unwrap())
+            .await
+            .context("error creating parent directory")?;
         {
-            let mut output_file = fs::File::create(&target_path).await?;
-            io::copy(&mut stream.content, &mut output_file).await?;
-            output_file.sync_all().await?;
+            let mut output_file = fs::File::create(dbg!(&target_path))
+                .await
+                .context("error creating file")?;
+            io::copy(&mut stream.content, &mut output_file)
+                .await
+                .context("error writing to file")?;
+            output_file
+                .sync_all()
+                .await
+                .context("error flushing file")?;
         }
     }
 
-    let (file_list, alg) =
-        add_path_into_remote_archive(storage, target_archive, &tempdir.path()).await?;
+    {
+        let mut dir = fs::read_dir(&tempdir.path()).await?;
 
-    fs::remove_dir_all(&tempdir).await?;
+        while let Some(entry) = dir.next_entry().await? {
+            dbg!(&entry.path());
+        }
+    };
+
+    let (file_list, alg) = add_path_into_remote_archive(storage, target_archive, &tempdir.path())
+        .await
+        .context("error adding into remote archive")?;
+
+    fs::remove_dir_all(&tempdir)
+        .await
+        .context("error cleaning tempdir")?;
 
     Ok((file_list, alg))
 }
@@ -76,7 +97,92 @@ async fn repackage_path(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::testing::TestEnvironment;
+    use docs_rs_types::testing::{KRATE, V1};
 
     // TODO:
     // * test with real S3 too so prefixes are handled properly
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_repackage_normal() -> Result<()> {
+        let env = TestEnvironment::new().await?;
+
+        const HTML_PATH: &str = "some/path.html";
+        const HTML_CONTENT: &str = "<html>content</html>";
+        const SOURCE_PATH: &str = "another/source.rs";
+        const SOURCE_CONTENT: &str = "fn main() {}";
+
+        let rid = env
+            .fake_release()
+            .await
+            .name(&KRATE)
+            .archive_storage(false)
+            .rustdoc_file_with(HTML_PATH, HTML_CONTENT.as_bytes())
+            .source_file(SOURCE_PATH, SOURCE_CONTENT.as_bytes())
+            .version(V1)
+            .create()
+            .await?;
+
+        let storage = env.storage()?;
+
+        // confirm we can fetch the files via old file-based storage.
+        assert_eq!(
+            storage
+                .stream_rustdoc_file(&KRATE, &V1, None, HTML_PATH, false)
+                .await?
+                .materialize(usize::MAX)
+                .await?
+                .content,
+            HTML_CONTENT.as_bytes()
+        );
+
+        assert_eq!(
+            storage
+                .stream_source_file(&KRATE, &V1, None, SOURCE_PATH, false)
+                .await?
+                .materialize(usize::MAX)
+                .await?
+                .content,
+            SOURCE_CONTENT.as_bytes()
+        );
+
+        // confirm the target archives really don't exist
+        let rustdoc_archive = rustdoc_archive_path(&KRATE, &V1);
+        let source_archive = source_archive_path(&KRATE, &V1);
+        for path in &[&rustdoc_archive, &source_archive] {
+            assert!(!storage.exists(path).await?);
+        }
+        dbg!(storage.list_prefix("").await.collect::<Vec<_>>().await);
+
+        repackage(&storage, &KRATE, &V1).await?;
+
+        dbg!(storage.list_prefix("").await.collect::<Vec<_>>().await);
+
+        // afterwards it work with archives.
+        assert!(
+            String::from_utf8_lossy(
+                &storage
+                    .stream_rustdoc_file(&KRATE, &V1, None, HTML_PATH, true)
+                    .await?
+                    .materialize(usize::MAX)
+                    .await?
+                    .content
+            )
+            .contains(HTML_CONTENT)
+        );
+
+        // assert_eq!(
+        //     &storage
+        //         .stream_source_file(&KRATE, &V1, None, SOURCE_PATH, true)
+        //         .await?
+        //         .materialize(usize::MAX)
+        //         .await?
+        //         .content,
+        //     SOURCE_CONTENT.as_bytes()
+        // );
+
+        // TODO: check if the release & build records were updated.
+
+        Ok(())
+    }
 }
