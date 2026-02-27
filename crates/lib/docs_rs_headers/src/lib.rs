cb24dfe2405e05abe41144c49d6f1a9a3488b69a
Denis Cornehl <denis@cornehl.org>
2026-02-27 15:34:13 +0100

    x-forwarded-proto

--- crates/lib/docs_rs_headers/src/lib.rs
+++ crates/lib/docs_rs_headers/src/lib.rs
@@ -1,6 +1,7 @@
 mod canonical_url;
 mod etag;
 mod forwarded_host;
+mod forwarded_proto;
 mod if_none_match;
 mod surrogate_key;
 #[cfg(test)]
@@ -9,6 +10,7 @@ mod testing;
 pub use canonical_url::CanonicalUrl;
 pub use etag::{ETagComputer, compute_etag};
 pub use forwarded_host::{X_FORWARDED_HOST, XForwardedHost};
+pub use forwarded_proto::{X_FORWARDED_PROTO, XForwardedProto};
 pub use headers::{ETag, Host};
 pub use if_none_match::IfNoneMatch;
 pub use surrogate_key::{SURROGATE_KEY, SurrogateKey, SurrogateKeys};
