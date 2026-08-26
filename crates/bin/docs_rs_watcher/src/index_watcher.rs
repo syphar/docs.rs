0be13cb441496ee5260333011646a2965ed74db3
Denis Cornehl <denis@cornehl.org>
2026-08-26 21:57:44 +0200

    kk

--- crates/bin/docs_rs_watcher/src/index_watcher.rs
+++ crates/bin/docs_rs_watcher/src/index_watcher.rs
@@ -187,13 +187,6 @@ async fn process_changes(
             // Generally we don't even start the git-index-watcher when
             // SQS is active.
             // Will be removed with the git index watcher code when SQS is stable.
-            metrics.event_processing_time.record(
-                start.elapsed().as_secs_f64(),
-                &[
-                    KeyValue::new("source", "git"),
-                    KeyValue::new("type", change_type),
-                ],
-            );
             continue;
         }
 
