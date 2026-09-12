9ee27571662543e9c593be84cde883a1d87f7c06
Denis Cornehl <denis@cornehl.org>
2026-09-12 11:30:41 +0200

    k

--- crates/bin/docs_rs_web/src/handlers/about.rs
+++ crates/bin/docs_rs_web/src/handlers/about.rs
@@ -131,4 +131,18 @@ mod tests {
         web.assert_success("/about").await?;
         Ok(())
     }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn nonexistent_about_page_is_cached() -> Result<()> {
+        let env = TestEnvironment::new().await?;
+        let response = env.web_app().await.get("/about/does-not-exist").await?;
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        response.assert_cache_control(
+            CachePolicy::ForeverInCdn(SURROGATE_KEY_DOCSRS_STATIC.into()),
+            env.config(),
+        );
+
+        Ok(())
+    }
 }
